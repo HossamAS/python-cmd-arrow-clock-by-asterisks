f0a812738413e2be58af765ea71535c7fcb1405e
hossam ali abdessalam <53792034+HossamAS@users.noreply.github.com>
2022-11-07 16:43:21 +0200

    Add files via upload

---
+++ clock-v1.py
@@ -0,0 +1,71 @@
+import time
+import os
+os.system('cls')
+i=1
+while i==1 : 
+    print("\n\n\n\n\n\n\n\n\n\
+    \n                                                      *\
+    \n                                                      * *\
+    \n                                                      * * *\
+    \n                                                      * * * *\
+    \n                                                      * * * * *\
+    \n                                * * * * * * * * * * * * * * * * *\
+    \n                                                      * * * * *\
+    \n                                                      * * * *\
+    \n                                                      * * *\
+    \n                                                      * *\
+    \n                                                      *")
+    time.sleep(0.3)
+    os.system('cls')
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                      * * * * * * * * * * *\
+    \n                        * * * * * * * * *\
+    \n                          * * * * * * *\
+    \n                            * * * * *\
+    \n                              * * *\
+    \n                                *")
+    time.sleep(0.3)
+    os.system('cls')
+    print("\n\n\n\n\n\n\n\n\n\
+    \n          *\
+    \n        * *\
+    \n      * * *\
+    \n    * * * *\
+    \n  * * * * *\
+    \n* * * * * * * * * * * * * * * * *\
+    \n  * * * * *\
+    \n    * * * *\
+    \n      * * *\
+    \n        * *\
+    \n          *")
+    time.sleep(0.3)
+    os.system('cls')
+    print("\
+                                *\
+    \n                              * * *\
+    \n                            * * * * *\
+    \n                          * * * * * * *\
+    \n                        * * * * * * * * *\
+    \n                      * * * * * * * * * * *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *\
+    \n                                *")
+    time.sleep(0.3)
+    os.system('cls')
